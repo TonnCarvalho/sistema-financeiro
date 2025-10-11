5d778067c4c320f7c82a3a8d38a5624503243e9f
Cleiton Carvalho <cleiton_601@hotmail.com>
2025-10-11 16:26:28 -0300

    feat: tema, adicionado temas light e dark

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
-import '@tabler/core/dist/js/tabler.js'
 import '../../node_modules/bootstrap/dist/js/bootstrap.js'
+import '@tabler/core/dist/js/tabler.js'
+
 
 import './blockArialHidden.js'
 
@@ -7,8 +8,10 @@ import Alpine from 'alpinejs'
 
 import contaBancaria from './alpine/contaBancaria'
 import investimento from './alpine/investimento'
+import tema from './alpine/tema'
 
 window.Alpine = Alpine
 Alpine.data('contaBancaria', contaBancaria)
 Alpine.data('investimento', investimento)
+Alpine.data('tema', tema)
 Alpine.start()
