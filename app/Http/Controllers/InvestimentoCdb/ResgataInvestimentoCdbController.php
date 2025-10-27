70466e4722f3c0d46a935dcccf6a1ff18be019a4
Cleiton Carvalho <cleiton_601@hotmail.com>
2025-10-27 13:43:15 -0300

    resgatando valores.

--- app/Http/Controllers/InvestimentoCdb/ResgataInvestimentoCdbController.php
+++ app/Http/Controllers/InvestimentoCdb/ResgataInvestimentoCdbController.php
@@ -8,6 +8,7 @@ use App\Models\InvestimentoExtrato;
 use App\Http\Controllers\Controller;
 use App\Models\InvestimentoExtratoDiario;
 use Illuminate\Support\Facades\Validator;
+use App\Services\ServicesGetInvestimentoValoresAtual;
 
 class ResgataInvestimentoCdbController extends Controller
 {
@@ -47,6 +48,13 @@ class ResgataInvestimentoCdbController extends Controller
                 ->withErrors($validator)
                 ->withInput();
         }
+
+        //Calculo dos valores
+        $investimento_valor_atual = ServicesGetInvestimentoValoresAtual::getValoresAtual($investimento->id);
+
+        $novo_valor_bruto = $investimento_valor_atual['valor_bruto'] - $request->resgata_valor;
+        $novo_valor_liquido = $investimento_valor_atual['valor_liquido'] - $request->resgata_valor;
+
         Investimento::find($investimento->id)
             ->decrementEach([
                 'valor_bruto' => $request->resgata_valor,
@@ -57,17 +65,23 @@ class ResgataInvestimentoCdbController extends Controller
             'user_id' => $this->userId,
             'investimento_id' => $investimento->id,
             'valor_aplicado' => '-' . $request->resgata_valor,
-            'valor_bruto' => '-' . $request->resgata_valor,
-            'valor_liquido' => '-' . $request->resgata_valor,
+            'valor_bruto' => $novo_valor_bruto,
+            'valor_liquido' => $novo_valor_liquido,
+            'ganho_perda' => $investimento_valor_atual['ganho_perda'],
+            'ir_iof' => $investimento_valor_atual['ir_iof'],
             'movimento' => 'saida'
         ]);
 
         InvestimentoExtratoDiario::create([
             'investimento_id' => $investimento->id,
             'investimento_extrato_id' => $investimentoExtrato->id,
+            'valor_bruto_diario' => $request->resgata_valor,
+            'valor_liquido_diario' => $request->resgata_valor,
             'created_at' => $request['data']
         ]);
+
         $request->session()->flash('success', "R$: {$request->resgata_valor}  reais resgatado com sucesso!");
-        return redirect()->route('investimento.show');
+        
+        return redirect()->route('investimento.show', $investimento->id);
     }
 }
